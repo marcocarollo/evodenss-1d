3aba06ea63a0457d948fb06d3e58ffca4881b722
marcocarollo <marco.carollo2000@gmail.com>
2025-03-04 10:37:52 +0100

    clear

--- evodenss/clear.sh
+++ evodenss/clear.sh
@@ -1,1 +1,31 @@
-rm ../results/mse_runs/used_*
+if [ -f "../results/mse_runs/used_config.yaml" ]; then
+    rm "../results/mse_runs/used_config.yaml"
+fi
+
+if [ -f "../results/mse_runs/used_grammar.yaml" ]; then
+    rm "../results/mse_runs/used_grammar.yaml"
+fi
+
+if [ -f "../results/CHLA/used_config.yaml" ]; then
+    rm "../results/CHLA/used_config.yaml"
+fi
+
+if [ -f "../results/CHLA/used_grammar.yaml" ]; then
+    rm "../results/CHLA/used_grammar.yaml"
+fi
+
+if [ -f "../results/NITRATE/used_config.yaml" ]; then
+    rm "../results/NITRATE/used_config.yaml"
+fi
+
+if [ -f "../results/NITRATE/used_grammar.yaml" ]; then
+    rm "../results/NITRATE/used_grammar.yaml"
+fi
+
+if [ -f "../results/BBP700/used_config.yaml" ]; then
+    rm "../results/BBP700/used_config.yaml"
+fi
+
+if [ -f "../results/BBP700/used_grammar.yaml" ]; then
+    rm "../results/BBP700/used_grammar.yaml"
+fi
